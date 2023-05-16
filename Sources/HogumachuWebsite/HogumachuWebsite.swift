883ff87ed480a8bd1b7ce737772ad6ff2067d89c
hogumachu <castle_jun@daum.net>
2023-05-16 21:49:23 +0900

    feat: change UI

--- Sources/HogumachuWebsite/HogumachuWebsite.swift
+++ Sources/HogumachuWebsite/HogumachuWebsite.swift
@@ -16,8 +16,8 @@ struct HogumachuWebsite: Website {
     }
 
     var url = URL(string: "https://hogumachu.github.io/")!
-    var name = "HogumachuBlog"
-    var description = "A description of HogumachuBlog"
+    var name = "호구마츄로그"
+    var description = "반갑습니다.\niOS 개발자 호구마츄입니다."
     var language: Language { .korean }
     var imagePath: Path? { nil }
     
