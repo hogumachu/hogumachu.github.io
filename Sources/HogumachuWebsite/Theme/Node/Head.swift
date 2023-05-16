2eaf820eb68572c60f84e47c4f0c4710d9b0cfcb
hogumachu <castle_jun@daum.net>
2023-05-16 16:54:42 +0900

    feat: add nodes

---
+++ Sources/HogumachuWebsite/Theme/Node/Head.swift
@@ -0,0 +1,11 @@
+import Foundation
+import Publish
+import Plot
+
+extension Node where Context == HTML.DocumentContext {
+    
+    static func headWithStylesheets<T: Website>(for location: Location, on site: T) -> Node {
+        return .head(for: location, on: site, stylesheetPaths: ["/Theme/styles.css"])
+    }
+    
+}
---
+++ Sources/HogumachuWebsite/Theme/Node/SiteHeader.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Publish
+import Plot
+
+struct SiteHeader<Site: Website>: Component {
+    var context: PublishingContext<Site>
+    var selectedSelectionID: Site.SectionID?
+
+    var body: Component {
+        Header {
+            Wrapper {
+                Link(context.site.name, url: "/")
+                    .class("site-name")
+
+                if Site.SectionID.allCases.count > 1 {
+                    navigation
+                }
+            }
+        }
+    }
+
+    private var navigation: Component {
+        Navigation {
+            List(Site.SectionID.allCases) { sectionID in
+                let section = context.sections[sectionID]
+
+                return Link(section.title,
+                    url: section.path.absoluteString
+                )
+                .class(sectionID == selectedSelectionID ? "selected" : "not-selected")
+            }
+        }
+    }
+}
