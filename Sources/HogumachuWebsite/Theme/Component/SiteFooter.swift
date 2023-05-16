a6dddaae035ba0f71e4a6daa1244499b41c1e925
hogumachu <castle_jun@daum.net>
2023-05-16 21:47:29 +0900

    chore: renamed (Node -> Component)

--- Sources/HogumachuWebsite/Theme/Component/SiteFooter.swift
+++ Sources/HogumachuWebsite/Theme/Component/SiteFooter.swift
@@ -6,11 +6,11 @@ struct SiteFooter: Component {
     var body: Component {
         Footer {
             Paragraph {
-                Text("Using:  ")
+                Text("Swift를 이용해 만들었어요 - ")
                 Link("Publish", url: "https://github.com/johnsundell/publish")
             }
             Paragraph {
-                Text("Copyright: ")
+                Text("저작권 - ")
                 Link("WTFPL", url: "http://www.wtfpl.net/")
             }
             Paragraph {
