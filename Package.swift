d91424dd24ee453c7bf457ca36a2f6a7d8328874
hogumachu <castle_jun@daum.net>
2024-03-28 15:20:05 +0900

    tokamakUI 추가

--- Package.swift
+++ Package.swift
@@ -3,22 +3,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "HogumachuWebsite",
-    platforms: [.macOS(.v12)],
-    products: [
-        .executable(
-            name: "HogumachuWebsite",
-            targets: ["HogumachuWebsite"]
-        )
-    ],
-    dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
-        .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0")
-    ],
-    targets: [
-        .executableTarget(
-            name: "HogumachuWebsite",
-            dependencies: ["Publish", "SplashPublishPlugin"]
-        )
-    ]
+  name: "HogumachuWebsite",
+  platforms: [.macOS(.v12)],
+  products: [
+    .executable(
+      name: "HogumachuWebsite",
+      targets: ["HogumachuWebsite"]
+    )
+  ],
+  dependencies: [
+    .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
+    .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0"),
+    .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", from: "0.11.1")
+  ],
+  targets: [
+    .executableTarget(
+      name: "HogumachuWebsite",
+      dependencies: [
+        "Publish",
+        "SplashPublishPlugin",
+        .product(name: "TokamakStaticHTML", package: "Tokamak")
+      ]
+    )
+  ]
 )
