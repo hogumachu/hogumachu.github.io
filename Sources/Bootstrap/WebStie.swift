4566bb4aa634aa5045aeb5c8b7252beb786b48bf
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-01-02 16:51:48 +0900

    feat: app/site 분리

---
+++ Sources/Bootstrap/WebStie.swift
@@ -0,0 +1,22 @@
+//
+//  WebStie.swift
+//  HogumachuWeb
+//
+//  Created by 홍성준 on 1/2/25.
+//
+
+import Foundation
+import Ignite
+
+// TODO: - WebSite 설정
+
+struct WebStie: Site {
+  let name = "Hogumachu 블로그"
+  let titleSuffix = " :: 🦧 iOS & Swift 🍌"
+  let url = URL(string: "https://hogumachu.github.io")!
+  let builtInIconsEnabled = true
+  let author = "Hogumachu"
+  let homePage = Home()
+  let theme: some Theme = MainTheme()
+  let pages: [any StaticPage] = []
+}
