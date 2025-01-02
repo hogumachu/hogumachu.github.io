6d6afdbcde80a502392438efb129a7ac6fc05c3b
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-01-02 15:09:33 +0900

    env: package 네이밍 변경

--- Package.swift
+++ Package.swift
@@ -4,14 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "IgniteStarter",
-    platforms: [.macOS(.v13)],
-    dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
-    ],
-    targets: [
-        .executableTarget(
-            name: "IgniteStarter",
-            dependencies: ["Ignite"]),
-    ]
+  name: "HogumachuWeb",
+  platforms: [.macOS(.v13)],
+  dependencies: [
+    .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+  ],
+  targets: [
+    .executableTarget(
+      name: "HogumachuWeb",
+      dependencies: ["Ignite"]
+    ),
+  ]
 )
