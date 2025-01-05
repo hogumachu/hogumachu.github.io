82c08f5edb53f18ef878af0d31e505afb6ba2827
SungJun Hong <castle_jun@daum.net>
2025-01-05 23:35:11 +0900

    image resource 추가

---
+++ Content/blog/sample.md
@@ -0,0 +1,11 @@
+---
+author: hogumachu
+description: for sample
+date: 2025-01-05 16:00
+tags: iOS, Swift, Blog
+published: true
+---
+
+# this is for sample
+
+this is for sample
--- Sources/DesignSystem/Image+Local.swift
+++ Sources/DesignSystem/Image+Local.swift
@@ -18,8 +18,32 @@ enum ImageResource: String {
   }
 }
 
+enum ContentBackgroundImageResource: String, CaseIterable {
+  case banana
+  case fire
+  case party
+  case perplex
+  case robot
+  case strawberry
+  
+  var path: String {
+    return "/images/contents/background/\(rawValue)-background.jpg"
+  }
+}
+
 extension Image {
   init(local resource: ImageResource) {
     self.init(resource.path, description: resource.rawValue + " image")
+  }
+  
+  init(background resource: ContentBackgroundImageResource) {
+    self.init(resource.path, description: resource.rawValue + " image")
+  }
+  
+  init(contentIndex index: Int) {
+    let count = ContentBackgroundImageResource.allCases.count
+    self = .init(
+      background: ContentBackgroundImageResource.allCases[index % count]
+    )
   }
 }
