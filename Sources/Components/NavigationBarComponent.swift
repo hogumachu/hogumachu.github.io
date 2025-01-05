697ba9fd312cb36ce942adc6e7d56dfe01be88b8
SungJun Hong <castle_jun@daum.net>
2025-01-05 15:35:58 +0900

    navigation bar body 추가

--- Sources/Components/NavigationBarComponent.swift
+++ Sources/Components/NavigationBarComponent.swift
@@ -11,11 +11,10 @@ import Ignite
 struct NavigationBarComponent: Component {
   func body(context: PublishingContext) -> [any PageElement] {
     NavigationBar(logo: logo) {
-      Link(target: "https://github.com/hogumachu") {
-        Image(resource: .github)
-          .resizable()
-          .foregroundStyle(.gray100)
-      }
+      linkComponent(BlogPage(), context: context)
+      linkComponent(ServicePage(), context: context)
+      linkComponent(CareerPage(), context: context)
+      additionalLinks
     }
     .navigationItemAlignment(.trailing)
     .navigationBarStyle(.dark)
@@ -24,8 +23,30 @@ struct NavigationBarComponent: Component {
   }
   
   private var logo: some InlineElement {
-    Text("🌎 Hogumachu Blog")
-      .font(.title3)
-      .foregroundStyle(.primaryColor)
+    Text("hogumachu tech")
+      .font(.title4)
+      .fontWeight(.regular)
+      .foregroundStyle(.textColor)
+  }
+  
+  private var additionalLinks: some NavigationItem {
+    Dropdown("More") {
+      linkComponent("GitHub", target: "https://github.com/hogumachu")
+      linkComponent("Tistory", target: "https://hogumachu.tistory.com")
+    }
+  }
+  
+  private func linkComponent(_ page: any StaticPage, context: PublishingContext) -> Link {
+    Link(page: page) {
+      Text(page.title)
+        .foregroundStyle(context.currentRenderingPath == page.path ? .primaryColor : .gray200)
+    }
+  }
+  
+  private func linkComponent(_ title: String, target: String) -> Link {
+    Link(target: target) {
+      Text(title)
+        .foregroundStyle(.gray200)
+    }
   }
 }
